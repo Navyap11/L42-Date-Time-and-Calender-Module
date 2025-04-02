6d3830657f984ef4e54a54eb83a40ec99689f0d8
navyap11 <ashwin.p.parmar@gmail.com>
2025-04-02 16:21:24 +0200

    d

---
+++ Date and Time.py
@@ -0,0 +1,8 @@
+from datetime import date, time, datetime
+date_of_today= date.today()
+time_right_now=datetime.now()
+
+print("Today's date is ",date_of_today)
+print("\nThe time right now is ",time_right_now)
+
+print("The year, month, and day of today is: ", date_of_today.year , date_of_today.month , date_of_today.day)
